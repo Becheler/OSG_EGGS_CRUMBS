21787667dd0091a8e223bbc17f45f4132a7749ee
Arnaud Becheler <arnaud.becheler@gmail.com>
2021-08-24 22:11:02 -0400

    backslash

--- DAG/generate_DAG.sh
+++ DAG/generate_DAG.sh
@@ -3,13 +3,13 @@
 for i in {1..5}
 do
    echo "JOB A$i A.condor"
-   echo "VARS A$i id="$i""
+   echo "VARS A$i id=\"$i\""
    echo "JOB B$i B.condor"
-   echo "VARS B$i id="$i""
+   echo "VARS B$i id=\"$i\""
    echo "JOB C$i C.condor"
-   echo "VARS C$i id="$i""
+   echo "VARS C$i id=\"$i\""
    echo "JOB D$i D.condor"
-   echo "VARS D$i id="$i""
+   echo "VARS D$i id=\"$i\""
    echo "PARENT A$i CHILD B$i"
    echo "PARENT B$i CHILD C$i"
    echo "PARENT C$i CHILD D$i"
