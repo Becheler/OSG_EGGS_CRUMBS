0e29b4d9ea3e6272cb6c28cfd37a1ed833d87d18
Arnaud Becheler <abechele@login04.osgconnect.net>
2021-08-24 21:09:53 -0500

    generate DAG

---
+++ DAG/generate_DAG.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+for i in {1..5}
+do
+   echo "JOB A$i A.condor"
+   echo "VARS A$i id="$i""
+   echo "JOB B$i B.condor"
+   echo "VARS B$i id="$i""
+   echo "JOB C$i C.condor"
+   echo "VARS C$i id="$i""
+   echo "JOB D$i D.condor"
+   echo "VARS D$i id="$i""
+   echo "PARENT A$i CHILD B$i"
+   echo "PARENT B$i CHILD C$i"
+   echo "PARENT C$i CHILD D$i"
+done
