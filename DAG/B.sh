0370f0fe5b45e33d82ebd32aae4b6a3dbfe36679
Arnaud Becheler <arnaud.becheler@gmail.com>
2021-09-01 00:20:41 -0400

    some more tests for non empty files

--- DAG/B.sh
+++ DAG/B.sh
@@ -3,18 +3,15 @@
 echoerr() { echo "$@" 1>&2; }
 
 rowids=$(python3 -m crumbs.get_successful_simulations_rowids --database $1 --table "quetzal_EGG_1" | tr -d '[],')
-echo "Rowids: $rowids"
-if [[ -z "$rowids" ]]; then 
-  echoerr $1": no newick formulas found in database."
-  exit 1
-else
-  echo  $1": newick formulas found in database."
+if ((${#rowids[@]})); then
+  echo  $1": newick formulas found in database, array is not empty."
   mkdir phylip
   for i in $rowids
   do
-    echo "Simulating sequence for Newick formula"$i"found in database "$1"."
+    echo "Rowid "$i" in database "$1": sampling s prior."
     s=$(python3 -m crumbs.sample "uniform_real" 0.00025 0.0000025)
-    echo "s = "$s
+    echo "Rowid "$i" in database "$1": s = "$s"."
+    echo "Rowid "$i" in database "$1": about to simulate PHYLIP sequences."    
     python3 -m crumbs.simulate_phylip_sequences \
     --database $1 \
     --table "quetzal_EGG_1" \
@@ -22,9 +19,19 @@ else
     --sequence_size 1041  \
     --scale_tree $s \
     --output "phylip/pod_"$i".phyl"
-    
-    echo "Databse "$1", rowid "$i": phylip/pod_"$i".phyl simulated"
+    if [ -s "phylip/pod_"$i".phyl" ]
+    then 
+      echo "Rowid "$i" in database "$1": PHYLIP file phylip/pod_"$i".phyl exists and is not empty "
+    else
+      echoerr "Rowid "$i" in database "$1": PHYLIP file phylip/pod_"$i".phyl does not exist, or is empty "
+      exit 1
+    fi
+    echo "Rowid "$i" in database "$1": phylip/pod_"$i".phyl simulated."
   done
-  tar -czf phylip.tar.gz phylip
-  echo "Databse "$1", phylip.tar.gz compressed."
+  echo "Database "$1", compressing PHYLIP files."
+  tar -czvf phylip.tar.gz phylip
+  echo "Database "$1", phylip.tar.gz created."
+else
+  echoerr $1": rowids array is empty: no newick formulas found in database."
+  exit 1
 fi
