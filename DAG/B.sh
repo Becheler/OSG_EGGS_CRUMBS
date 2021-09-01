3b72a13a6d52186855bf87123b5792aed0c997f0
Arnaud Becheler <arnaud.becheler@gmail.com>
2021-09-01 00:32:32 -0400

    nicer output

--- DAG/B.sh
+++ DAG/B.sh
@@ -4,14 +4,14 @@ echoerr() { echo "$@" 1>&2; }
 
 rowids=$(python3 -m crumbs.get_successful_simulations_rowids --database $1 --table "quetzal_EGG_1" | tr -d '[],')
 if ((${#rowids[@]})); then
-  echo  $1": newick formulas found in database, array is not empty."
+  echo  "Database "$1": newick formulas found in database, array is not empty, iteration possible. Creating folder phylip."
   mkdir phylip
   for i in $rowids
   do
-    echo "Rowid "$i" in database "$1": sampling s prior."
+    echo "Database "$1", rowid "$i": sampling s prior."
     s=$(python3 -m crumbs.sample "uniform_real" 0.00025 0.0000025)
-    echo "Rowid "$i" in database "$1": s = "$s"."
-    echo "Rowid "$i" in database "$1": about to simulate PHYLIP sequences."    
+    echo "Database "$1", rowid "$i": s = "$s"."
+    echo "Database "$1", rowid "$i": about to simulate PHYLIP sequences."    
     python3 -m crumbs.simulate_phylip_sequences \
     --database $1 \
     --table "quetzal_EGG_1" \
@@ -21,17 +21,17 @@ if ((${#rowids[@]})); then
     --output "phylip/pod_"$i".phyl"
     if [ -s "phylip/pod_"$i".phyl" ]
     then 
-      echo "Rowid "$i" in database "$1": PHYLIP file phylip/pod_"$i".phyl exists and is not empty "
+      echo "Database "$1", rowid "$i": PHYLIP file phylip/pod_"$i".phyl exists and is not empty. Continuing iteration."
     else
-      echoerr "Rowid "$i" in database "$1": PHYLIP file phylip/pod_"$i".phyl does not exist, or is empty "
+      echo "Database "$1", rowid "$i": PHYLIP file phylip/pod_"$i".phyl does not exist, or is empty. Exiting iteration with code 1."
+      echoerr "Database "$1", rowid "$i": PHYLIP file phylip/pod_"$i".phyl does not exist, or is empty. Exiting iteration with code 1."
       exit 1
     fi
-    echo "Rowid "$i" in database "$1": phylip/pod_"$i".phyl simulated."
   done
-  echo "Database "$1", compressing PHYLIP files."
+  echo "Database "$1": compressing PHYLIP files."
   tar -czvf phylip.tar.gz phylip
-  echo "Database "$1", phylip.tar.gz created."
+  echo "Database "$1": archive phylip.tar.gz created."
 else
-  echoerr $1": rowids array is empty: no newick formulas found in database."
+  echoerr "Database "$1": rowids array is empty. No newick formulas found in database. Exiting with code 1."
   exit 1
 fi
