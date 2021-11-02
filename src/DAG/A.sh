9ce9b7e4d18d21f22d5da364aded39477c6e6143
Arnaud Becheler <abechele@login04.osgconnect.net>
2021-11-02 16:02:42 -0500

    time threshold down to 1000 g

--- src/DAG/A.sh
+++ src/DAG/A.sh
@@ -5,7 +5,7 @@ N=$(python3 -m crumbs.sample "uniform_integer" 10 100000)
 K=$(python3 -m crumbs.sample "uniform_integer" 10 100000)
 r=$(python3 -m crumbs.sample "uniform_real" 1 5)
 m=$(python3 -m crumbs.sample "uniform_real" 0.0 0.5)
-g=$(python3 -m crumbs.sample "uniform_integer" 500 2000)
+g=$(python3 -m crumbs.sample "uniform_integer" 500 1000)
 p=$(python3 -m crumbs.sample "uniform_real" 0.0001 0.5)
 latlon=($(python3 -m crumbs.sample "uniform_latlon" "suitability.tif" | tr -d '[],'))
 
